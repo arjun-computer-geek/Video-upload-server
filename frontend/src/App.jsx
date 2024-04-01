ede2f2971bde089441cc49064cb292afa50b0e7e
Arjun kumar <arjun.k@zyapaar.com>
2024-04-01 15:05:00 +0530

    inital setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./components/Header";
+
+// import { Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <Header />
+      {/* <Routes>
+        <Route path="/" element={<h1>Hi</h1>} />
+      </Routes> */}
+    </>
+  );
+}
+
+export default App;
